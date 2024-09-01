ae8eb2d9d9ce7d3aa2cd5a197170978d67c63b84
alzipapa <albert.ndemi@proton.me>
2024-09-01 23:37:09 +0300

    Intro to python assignment
    Create a new Python file and name it "user_input.py"
    Use the input() function to ask the user for their name and store it in a variable called "name".
    Use the input() function to ask the user for their age and store it in a variable called "age".
    Use the input() function to ask the user for their location and store it in a variable called "location".
    Print out a personalized message using the user's name, age, and location. For example: "Hello [name], you are [age] years old and live in [location]."
    Save and run the program to see the output.

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# Ask the user for their name and store it in a variable
+name = input('Please enter your name: ')
+
+# Ask the user for their age and stor it in a variable
+age = input('Please enter your age: ')
+
+# Ask the user for their location and store it in a variable
+location = input('Please enter your location: ')
+
+# Print out a personalized message
+print(f'Hallo {name}, you are {age} years old and live in {location}.')
+
